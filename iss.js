550d19168b525892c06f99992a581b54d3c2ad61
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-26 18:16:57 -0700

    Commit iss js

--- iss.js
+++ iss.js
@@ -32,4 +32,19 @@ const fetchCoordsByIP = function(ip, callback) {
   })
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url, function(error, response, body) {
+  if (error) {
+    callback(error, null);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+  }
+  const passes = JSON.parse(body).response;
+  callback(null, passes);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
