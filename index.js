287cdb241cd44e4a403d798e3291d8b6fc9b6630
Mrdo Mrdo <mayilyane9@gmail.com>
2021-08-26 18:17:18 -0700

    Commit index js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -16,4 +16,14 @@ fetchCoordsByIP('24.84.16.187', function(error, coordinates) {
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const theCoords = { latitude: '49.27670', longitude: '-123.13000' };
+fetchISSFlyOverTimes(theCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
